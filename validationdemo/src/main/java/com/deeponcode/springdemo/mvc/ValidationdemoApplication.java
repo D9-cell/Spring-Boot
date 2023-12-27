50afb071407921e241c4b0c506d004fad396720f
DEEPON DAS <deeponsubhro@gmail.com>
2023-12-27 12:09:01 +0530

    Add files via upload

---
+++ validationdemo/src/main/java/com/deeponcode/springdemo/mvc/ValidationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.deeponcode.springdemo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
